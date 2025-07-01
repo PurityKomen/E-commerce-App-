0d6ae1a212576c55a573dab381db5ee92a099fd8
PurityKomen <jepkorirpurity8@gmail.com>
2025-07-01 15:38:48 +0300

    added port

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv'
 
-const app = express;
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('server is running')
+})
